2b2bdf4e589767bad04b112d9899bba228d0a999
Gustavo Morales <gustavo.morales@gmail.com>
2015-12-03 13:50:51 -0500

    Add Post's author

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,10 +26,11 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
